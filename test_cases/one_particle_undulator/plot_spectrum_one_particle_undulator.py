eccc8b05d99ffb93c48fda38b4695e96f9b28f32
Dominika <dominika.maslarova@gmail.com>
2024-04-10 18:28:32 +0200

    updating test_case files

--- test_cases/one_particle_undulator/plot_spectrum_one_particle_undulator.py
+++ test_cases/one_particle_undulator/plot_spectrum_one_particle_undulator.py
@@ -1,24 +1,26 @@
+# Import libraries and data info from get_trajectory_one_particle_undulator script
 import matplotlib.pyplot as plt
 import matplotlib
-from get_trajectories_one_particle_undulator import *
-from scipy.signal import savgol_filter
+from get_trajectory_one_particle_undulator import *
+
+# Read data from FIKA simulation:
 with h5py.File('test_cases/one_particle_undulator/final_spectrum.h5', 'r') as file:
-    ene        = np.array(file['ene'])
-    spectrum    = np.array(file['spectrum_ene'])
-    signal_time = np.array(file['t'])
-    signal_y    = np.array(file['spectrum_t'])
+    ene          = np.array(file['ene'])
+    spectrum_ene = np.array(file['spectrum_ene'])
+    signal_time  = np.array(file['t'])
+    spectrum_t   = np.array(file['spectrum_t'])
 
-def round_to_odd(f):
-  return int(np.ceil(f) // 2 * 2 + 1)
-
+# Plot temporal profile in the observer place. Parameters r = 1, phi = 0, theta = pi/2 were used in input.py.
+# Other parameters used were E_slice_eV = 0.01, t_slice_s = 1e-18, E_radMax_eV = 20.
 fig, ax = plt.subplots()
-plt.title(r'$E_{tot}} = $ 10 MeV, $r_{\beta}$ = 0.05 $\rm{\mu m}$',fontsize=14)
-plt.plot(signal_time / 1e-15, signal_y)
+# Write important into into the title
+plt.title(r'$E_{e} = $10 MeV, $r_{\beta}$ = 0.05 $\rm{\mu m}$', fontsize=14)
+# Plot temporal profile, time is in the units of fs
+plt.plot(signal_time / 1e-15, spectrum_t)
+# Put x & y labels
 plt.xlabel('$t$ [fs]', fontsize = 14)
 plt.ylabel(r'$\frac{d^2 W}{\rm{d}t \rm{d}\Omega}$ [$\rm{J}$ $\rm{s}^{-1}$ $\rm{sr}^-1$]', fontsize=14)
-
-#spectrum_smoothed_w = savgol_filter(signal_y, round_to_odd((len(signal_y))/50), 3)
-#plt.plot(signal_time / 1e-15, spectrum_smoothed_w, color = 'darkblue',linestyle = 'dashed', label = 'smoothed data weighted') 
+#Format ticks fontsize, position and orientation
 mf = matplotlib.ticker.ScalarFormatter(useMathText=True)
 plt.gca().yaxis.set_major_formatter(mf)
 ax.yaxis.offsetText.set_fontsize(14)
@@ -26,30 +28,31 @@ plt.xticks(fontsize = 14)
 plt.yticks(fontsize = 14)
 ax.tick_params(axis="x",direction="in", bottom=True, top=True, left=True, right=True)
 ax.tick_params(axis="y",direction="in", bottom=True, top=True, left=True, right=True)
+# Fit figure to the figure box and save
 plt.tight_layout()
+plt.savefig('test_cases/one_particle_undulator/temporal_profile.png')
 
-#plt.savefig('figs/test_case_1_temporal_profile.pdf')
-
-K       = 1.33e-10 * sqrt(gamma*n_e) * r_beta
-omega_1 = 2 * gamma**2 * omega_beta/ (1 + K**2 / 2)
-print('Expected fundamental frequency according to formula 2*gamma^2*omega_beta/1+K^2/2, where K = 1.33*10^-10*r_beta*(gamma*n_e)^0.5 is ' + str(round(omega_1*hbar/e,1))+ 'eV')
-max_s = np.argmax(spectrum)
-print(round(ene[max_s],2))
-
-
+# Plot energy spectrum
 fig, ax = plt.subplots()
-plt.plot(ene, spectrum / 1e-21, label = 'raw data') 
-plt.title(r'$E_{tot} = $ 10 MeV, $r_{\beta}$ = 0.05 $\rm{\mu m}$',fontsize=14)
+plt.plot(ene, spectrum_ene / 1e-21) 
+plt.title(r'$E_{e} = $10 MeV, $r_{\beta}$ = 0.05 $\rm{\mu m}$',fontsize=14)
 ax.tick_params(axis="x",direction="in", bottom=True, top=True, left=True, right=True)
 ax.tick_params(axis="y",direction="in", bottom=True, top=True, left=True, right=True) 
 plt.xlabel('$E_{ph}$ [eV]', fontsize=14)
-plt.ylabel(r'$\frac{d^2 I}{d\omega d\Omega}$ [$10^{-21}$ $\rm{J}$ $\rm{eV}^{-1}$ $\rm{sr}^-1$]', fontsize=14)
+plt.ylabel(r'$\frac{d^2 W}{d\omega d\Omega}$ [$10^{-21}$ $\rm{J}$ $\rm{eV}^{-1}$ $\rm{sr}^-1$]', fontsize=14)
 plt.xticks(fontsize = 14)
 plt.yticks(fontsize = 14)
 plt.xlim([0,50])
 plt.ylim([0,10])
 plt.tight_layout()
-#plt.savefig('figs/test_case_1_spectrum.pdf')
-#plt.savefig('figs/test_case_1_spectrum.png')
+plt.savefig('test_cases/one_particle_undulator/energy_spectrum.png')
+
+# Calculate expected outcome with analytical formula and compare it with the code outcome.
+K       = 1.33e-10 * sqrt(gamma*n_e) * r_beta
+omega_1 = 2 * gamma**2 * omega_beta/ (1 + K**2 / 2)
+max_s   = np.argmax(spectrum_ene)
+print('Expected fundamental frequency according to formula 2*gamma^2*omega_beta/(1+K^2/2), where K = 1.33*10^-10*r_beta*(gamma*n_e)^0.5 is ' + str(round(omega_1*hbar/e,1))+ 'eV.')
+print('The fundamental frequency in the spectrum calculated by FIKA is ' +str(round(ene[max_s],1))+' eV.')
+
 plt.show()
 
